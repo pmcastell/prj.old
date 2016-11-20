8623d6fa2dad23016f5c5d9da65bc47b6610f013
regg6543 <reg6543@gmail.com>
2016-11-20 20:22:41 +0100

    Actualización 2016-11-20:19:22

--- scripts/firewall.sh
+++ scripts/firewall.sh
@@ -1,36 +1,40 @@
 sudo iptables -F
 sudo iptables -t nat -F
 sudo iptables -t mangle -F
-ACEPTAR=$(param -accept $*)
-BORRAR=$(param -drop $*)
+ACCEPT=$(param -accept $@)
+DROP=$(param -drop $@)
+MASQUERADE=$(param -masquerade $@)
 CURSO=""
-ACEPTAR="172.16.1.0/27 192.168.76.0/29 10.9.9.0/24  10.1.1.0/24 172.16.145.0/24 172.16.3.0 172.16.4.0 172.16.6.0 172.16.14/24 192.168.8.69 192.168.1.1 192.168.0.1 192.168.0.2 192.168.8.1 172.16.254.0/24 192.168.1.104 192.168.0.194 172.16.0.1"$ACEPTAR
-if [ "$1" != "" -a "$1" != "-accept" -a "$1" != "-drop" ];
-then
+ACCEPT="172.16.1.0/27 
+192.168.76.0/29 
+10.9.9.0/24  
+172.16.145.0/24 
+192.168.1.1 
+192.168.0.1 
+172.16.254.0/24 
+172.18.161.0/24
+172.18.162.0/24
+192.168.10.0/24
+$ACCEPT"
+
+if [ "$1" != "" -a "$1" != "-accept" -a "$1" != "-drop" ]; then
    CURSO=$1
 fi
-if [ "$CURSO" = "DAM1" ];
-then
-   ACCEPT="172.16.4.0/24 "$ACEPTAR
-elif [ "$CURSO" = "SMR1" ];
-then
-   ACCEPT="172.16.2.0/24 "$ACEPTAR
-   echo smr1
-elif [ "$CURSO" = "SMR2" ];
-then   
-   ACCEPT="172.16.3.0/24 "$ACEPTAR
-elif [ "$CURSO" = "DAM2" ];
-then
-   ACCEPT="172.16.6.0/24 "$ACEPTAR   
-elif [ "$CURSO" = "ESO" ];
-then
-   ACCEPT="192.168.254.0/24 "$ACEPTAR
+if [ "$CURSO" = "DAM1" ]; then
+   ACCEPT="172.16.4.0/24 "$ACCEPT
+elif [ "$CURSO" = "SMR1" ]; then
+   ACCEPT="172.16.2.0/24 "$ACCEPT
+elif [ "$CURSO" = "SMR2" ]; then   
+   ACCEPT="172.16.3.0/24 "$ACCEPT
+elif [ "$CURSO" = "DAM2" ]; then
+   ACCEPT="172.16.6.0/24 "$ACCEPT   
+elif [ "$CURSO" = "ESO" ]; then
+   ACCEPT="192.168.254.0/24 "$ACCEPT
 else
-   echo aceptar: $ACEPTAR
-   ACCEPT=$ACEPTAR   
+   echo ACEEPT: $ACCEPT; ACCEPT=$ACCEPT;   
 fi
-DROP="192.168.0.0/16 172.16.0.0/16 "$BORRAR
-MASQUERADE="10.9.9.0/24 192.168.44.0/24 10.1.1.0/24 172.16.145.0/24 "
+DROP="192.168.0.0/16 172.16.0.0/16 $DROP"
+MASQUERADE="10.9.9.0/24 192.168.44.0/24 10.1.1.0/24 172.16.145.0/24 $MASQUERADE"
 for a in $ACCEPT; do
    sudo iptables -A INPUT -j ACCEPT -s $a
 done
@@ -47,11 +51,12 @@ for a in $MASQUERADE; do
    sudo iptables -A POSTROUTING -t nat -j MASQUERADE -s $a
 done
 echo 1 >  /proc/sys/net/ipv4/ip_forward 
+sudo iptables -nL
+sudo iptables -t nat -nL
+
 #sudo iptables -A PREROUTING -t nat -p tcp -m tcp -d 192.168.1.6/32 --dport 25 -j DNAT --to-destination 10.10.10.9:25
 #sudo iptables -A OUTPUT -m owner --gid-owner noi -j DROP
 #sudo iptables -A OUTPUT -m owner --uid noi -j DROP
-sudo iptables -nL
-sudo iptables -t nat -nL
 
 #sudo iptables -A POSTROUTING -t nat -j MASQUERADE -s 172.16.71.0/24 -o ath1
 #sudo iptables -A PREROUTING -t nat -p tcp -m tcp -d 192.168.1.6/32 --dport 22 -j DNAT --to-destination 10.10.10.9:25
