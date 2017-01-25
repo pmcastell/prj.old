3d616bec3c21728cc347a711205e4bbe1d854a5d
regg6543 <reg6543@gmail.com>
2017-01-25 20:14:19 +0100

    Actualización 2017-01-25:19:14

--- scripts/aula/resetRouterTelef.sh
+++ scripts/aula/resetRouterTelef.sh
@@ -8,5 +8,7 @@ IESN_CONSERG="192.168.1.105"
 IESN_ENGLISH_DEPARTAMENT="192.168.1.107"
 IESN_DPTO_INFORMATICA="192.168.1.109"
 wget --timeout=5 --tries=2 --user=1234 --password=1234 "http://192.168.1.102/Forms/DiagGeneral_2?IsReset=1"
-wget --timeout=5 --tries=2 --user=1234 --password=1234 "http://192.168.1.107/webconfig/commit_reboot/commit_reboot.html/CommitReboot?rebootfrom=RBF5&reboot=Reboot"
-wget --timeout=5 --tries=2 --user=1234 --password=1234 "http://192.168.1.109/webconfig/commit_reboot/commit_reboot.html/CommitReboot?rebootfrom=RBF5&reboot=Reboot"
+for i in 107 109; do
+   wget --timeout=5 --tries=2 --user=1234 --password=1234 -O - "http://192.168.1.$i/webconfig/commit_reboot/commit_reboot.html/CommitReboot?rebootfrom=RBF5&reboot=Reboot" 
+done
+#wget --timeout=5 --tries=2 --user=1234 --password=1234 "http://192.168.1.109/webconfig/commit_reboot/commit_reboot.html/CommitReboot?rebootfrom=RBF5&reboot=Reboot"
