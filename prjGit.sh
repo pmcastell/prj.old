bca3827a6260533ce8d6474959e8cc4e7d9ec0d8
regg6543 <reg6543@gmail.com>
2017-03-27 21:49:05 +0200

    Actualización 2017-03-27:19:49

--- prjGit.sh
+++ prjGit.sh
@@ -6,6 +6,6 @@ if [ ! -d $DEST ]; then
    cd $DEST
    git clone https://github.com/javier-iesn/prj
 fi  
-cp -rv /m/Mios/Instituto/prj $DEST/
+rsync -rv /m/Mios/Instituto/prj $DEST/
 cd $DEST/prj
 /m/Mios/prj/scripts/git.sh commit
