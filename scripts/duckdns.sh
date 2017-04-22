649130d4d4e42338a1eb352b030e6cec43ea3d82
regg6543 <reg6543@gmail.com>
2017-04-22 22:01:54 +0200

    Actualización 2017-04-22:20:01

---
+++ scripts/duckdns.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#https://www.duckdns.org/update?domains={YOURVALUE}&token={YOURVALUE}[&ip={YOURVALUE}][&ipv6={YOURVALUE}][&verbose=true][&clear=true]
+[ "$1" = "" ] && DOMAIN="paquita58"
+[ "$2" = "" ] && IP=$(realIp) || IP="$2"
+TOKEN="03302f67-1c64-40be-8554-d35028f5f3af"
+#echo wget -O - "https://www.duckdns.org/update?domains=${DOMAIN}&token=${TOKEN}&ip=${IP}&verbose=true"
+#wget -O - "https://www.duckdns.org/update?domains=${DOMAIN}&token=${TOKEN}&ip=${IP}&verbose=true" 2> /dev/null
+echo wget -O - "https://www.duckdns.org/update?domains=paquita58&token=${TOKEN}&ip=${IP}&verbose=true" 2>/dev/null
+wget -O - "https://www.duckdns.org/update?domains=paquita58&token=${TOKEN}&ip=${IP}&verbose=true" 2>/dev/null
