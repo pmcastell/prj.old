89b52ad1db4ded0f4c23457ce8dff85661551299
regg6543 <reg6543@gmail.com>
2017-07-12 21:29:33 +0200

    Actualización 2017-07-12:19:29

--- scripts/githubGet.sh
+++ scripts/githubGet.sh
@@ -1,1 +1,6 @@
-wget -c https://raw.githubusercontent.com/javier-iesn/prj/master/scripts/$1
+#!/bin/bash
+if [ "$1" != "" ]; then
+   wget -c https://raw.githubusercontent.com/javier-iesn/prj/master/scripts/$1
+else
+   wget -cr https://raw.githubusercontent.com/javier-iesn/prj/master/scripts/
+fi
---
+++ scripts/githubGetSubdir.sh
@@ -0,0 +1,1 @@
+svn export https://github.com/javier-iesn/prj/trunk/scripts
