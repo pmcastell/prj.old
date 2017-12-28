c7059d01a50b8c08fd3295c3f3873e2fa98f287f
regg6543 <reg6543@gmail.com>
2017-12-28 20:27:43 +0100

    Actualización 2017-12-28:19:27

--- scripts/iwApd.sh
+++ scripts/iwApd.sh
@@ -6,7 +6,7 @@ uso() {
 
 crearIface() {
    local IFACE="$1" NAME="$2" TYPE="$3"
-   sudo iw dev $IFACE interface add $NAME type $TYPE
+   eecho sudo iw dev $IFACE interface add $NAME type $TYPE
    if [ "$(ifconfig -a | grep $NAME)" = "" ]; then 
       MAC=$(ip -o link | grep "${IFACE}:" | awk '{print $(NF-2);}')
       IFACE2=$(ip -o link | grep $MAC | grep -v $IFACE | awk -F':' '{print $2;}')
@@ -25,15 +25,15 @@ reinicia() {
 [ "$1" = "" ] && uso
 IFACE="$1"
 [ "$2" = "" ] && IP="192.168.1.27/24" || IP="$2"
-sudo ip link set dev $IFACE down
-crearIface $IFACE ${IFACE}0 station
-sudo macchanger -r ${IFACE}0
+###sudo ip link set dev $IFACE down
+###crearIface $IFACE ${IFACE}0 station
+###sudo macchanger -r ${IFACE}0
 crearIface $IFACE ${IFACE}1 __ap
 sudo macchanger -r ${IFACE}1
-sudo ip a add $IP dev ${IFACE}0
-sudo ip link set dev ${IFACE}0 up
-sudo wpa_supplicant -B -i ${IFACE}0 -c /m/Mios/Personal/AIRELAB/wicd/MOVISTAR_E360 -Dwext &
-sudo ip route add default via "$(echo $IP | awk -F'/' '{print $1;}' | cut -d'.' -f1-3)".1
+###sudo ip a add $IP dev ${IFACE}0
+###sudo ip link set dev ${IFACE}0 up
+###sudo wpa_supplicant -B -i ${IFACE}0 -c /m/Mios/Personal/AIRELAB/wicd/MOVISTAR_E360 -Dwext &
+###sudo ip route add default via "$(echo $IP | awk -F'/' '{print $1;}' | cut -d'.' -f1-3)".1
 sudo /scripts/finPuntoAcceso.sh
 sudo /scripts/puntoAcceso.sh ${IFACE}1 MiCasa22 Montoro65 172.16.1 35 128
 
