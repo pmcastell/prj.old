52809c0350d5a01fcd6d5503d52f6b0a274fd82d
usuario <fran.nucia@gmail.com>
2018-11-29 01:19:47 +0100

    Actualización 2018-11-29:00:19

--- scripts/firefox.sh
+++ scripts/firefox.sh
@@ -1,27 +1,6 @@
-PID=$(ps ax | grep -i /home/usuario/programas/firefox/firefox | grep -v grep | awk '{print $1;}')
-if [ "$PID" = "" ];
-then
-   while [ "$PID" = "" ];
-   do
-      if [ -f /usr/bin/firefox-3.5 ];
-      then      
-	/usr/bin/firefox-3.5 &
-      fi
-      sleep 1
-      PID=$(ps ax | grep -i firefox-3.5 | grep -v grep | awk '{ print $1;}' | head -1)
-   done     
-   PID=$(ps ax | grep -i firefox-3.5 | grep -v grep | awk '{ print $1;}' | head -1)
-   I=1
-
-   while [ "$PID" != "" ];
-   do
-      kill -9 $PID
-      if [ $I -gt 1 ];
-      then
-         sleep 1;
-      fi
-      I=$( expr $i + 1 )
-      PID=$(ps ax | grep -i firefox-3.5 | grep -v grep | awk '{ print $1;}' | head -1)
-   done 
-fi     
-/home/usuario/programas/firefox/firefox $1 $2 $3 $4 $5 $6 $7 $8 $9 &
+#/usr/bin/firefox --new-window &
+#sleep 5
+/usr/bin/firefox --new-window --private file:///m/Mios/Instituto/horarios/html/agenda.html "$@"  &
+[ -e /tmp/keypass2.lock ] && echo salgo && exit 0
+[ "$(pgrep keypass2)" = "" ] && [ ! -e /tmp/keypass2.lock ] && touch /tmp/keypass2.lock && /usr/bin/keepass2 
+rm /tmp/keypass2.lock
