da5922fdaca2d9e1d304e21533e288ec20090b96
usuario <fran.nucia@gmail.com>
2018-12-23 14:53:34 +0100

    Actualización 2018-12-23:13:53

--- scripts/aula/srvOn.sh
+++ scripts/aula/srvOn.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+main() {
+    if [ "$1" = "" ]; then uso; fi
+    while [ "$1" != "" ]; do
+        [ "$1" = "ALL" ] && BUSCA="" || BUSCA="$1"
+#        echo "BUSCA: $BUSCA"
+        MACS="$(macsConocidas $BUSCA | awk '{print $2;}')"
+#        echo "MACS: $MACS"
+        IFACES="$(_ifaces)"
+#        echo "IFACES: $IFACES"
+        for MAC in $MACS; do
+            for IFACE in $IFACES; do 
+                echo sudo etherwake -i $IFACE $MAC
+                sudo etherwake -i $IFACE $MAC
+            done
+        done
+        shift
+    done
+}    
+
 uso() {
    echo uso: $0 '<servidor-a-despertar>'
    echo Servidores disponibles: 
@@ -11,38 +30,53 @@ uso() {
          DEPMAT2
          PT1
          DeptSociales1
+         paula1
+         paula2
+         dpto
          ALL \(TODOS\)"
    exit 1
 }
 todas() {
-   MACS="00:24:01:ee:22:59 00:24:01:ed:30:c1 00:e0:4c:38:00:73 00:19:99:88:6a:f8 c4:6e:1f:05:fc:71 00:19:99:80:53:e1 00:0f:ea:40:57:3f 00:24:01:ee:21:c7 44:8a:5b:c2:77:fc 44:8a:5b:c2:71:e6 8c:89:a5:2e:88:ba"
-   for MAC in $MACS; do 
-      echo sudo etherwake -i eth1 $MAC;
-      sudo etherwake -i eth1 $MAC;
+    MACS="$(macsConocidas | awk '{print $2;}')"
+    IFACES="$(_ifaces)"
+    for MAC in $MACS; do 
+        for IFACE in $IFACES; do
+            echo sudo etherwake -i $IFACE $MAC;
+            sudo etherwake -i $IFACE $MAC;
+        done
    done
-}   
-if [ "$1" = "" ]; then uso; fi
-if [ "$(whoami)" != "root" ]; then sudo $0 $@; exit $?; fi
-while [ "$1" != "" ]; do
-   if [ "$1" = "srvSalaProfes" ];   then MAC="00:24:01:ee:22:59";
-   elif [ "$1" = "aula2srv" ];      then MAC="00:24:01:ed:30:c1 00:e0:4c:38:00:73";
-   elif [ "$1" = "aula1srv" ];      then MAC="00:19:99:88:6a:f8 c4:6e:1f:05:fc:71";
-   elif [ "$1" = "tecnologia" ];    then MAC="00:19:99:80:53:e1";
-   elif [ "$1" = "biblioSrv1" ];    then MAC="00:24:01:ee:21:c7";
-   elif [ "$1" = "DEPMAT2" ];       then MAC="44:8a:5b:c2:77:fc";
-   elif [ "$1" = "PT1" ];           then MAC="44:8a:5b:c2:71:e6";
-   elif [ "$1" = "Salaprofes2" ];   then MAC="00:0f:ea:40:57:3f";
-   elif [ "$1" = "DeptSociales1" ]; then MAC="8c:89:a5:2e:88:ba";
-   elif [ "$1" = "ALL" ];         then todas; exit 0;
-   fi
-   for M in $MAC; do
-      sudo etherwake -i eth1 $M
-      sudo etherwake -i eth0 $M
-   done
-   shift
-done
-exit 0
-if [ "$MAC" != "" ]; then echo sudo etherwake -i eth1 $MAC; sudo etherwake -i eth1 $MAC;
-else echo Error servidor no encontrado; fi
+} 
+macsConocidas() {
+    __macsConocidas() {
+          echo "srvSalaProfes   00:24:01:ee:22:59
+aula2srv        00:24:01:ed:30:c1 
+aula2srv        00:e0:4c:38:00:73 
+aula1srv        00:19:99:88:6a:f8 
+aula1srv        c4:6e:1f:05:fc:71 
+tecnologia      00:19:99:80:53:e1 
+biblioSrv1      00:24:01:ee:21:c7 
+DEPMAT2         44:8a:5b:c2:77:fc 
+PT1             44:8a:5b:c2:71:e6 
+Salaprofes2     00:0f:ea:40:57:3f 
+DeptSociales1   8c:89:a5:2e:88:ba
+paula1          00:19:99:88:6a:f8
+paula2          00:24:01:ed:30:c1
+dpto            20:cf:30:90:dc:5b" 
+    }
+    if   [ "$1" = "" ];   then  __macsConocidas
+    elif [ "$1" = "-m" ]; then (__macsConocidas | awk '{print $2;}')
+    elif [ "$1" = "-n" ]; then (__macsConocidas | awk '{print $1;}')
+    elif [ "$1" != "" ];  then (__macsConocidas | grep "$1" )
+    fi
+#    | grep "$1" | awk '{print $2;}'
+}
+_ifaces() {
+    ip -o l | awk '{print $2;}' | uniq | cut -d':' -f 1
+}        
+(return 2>/dev/null) || main "$@"
+#exit 0
 
-   
+###if [ "$MAC" != "" ]; then echo sudo etherwake -i eth1 $MAC; sudo etherwake -i eth1 $MAC;
+###else echo Error servidor no encontrado; fi
+
+
