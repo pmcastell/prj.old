9fb37fbe9701a43628ee636fd3a36ba05063ce07
usuario <fran.nucia@gmail.com>
2019-01-18 22:10:41 +0100

    Actualización 2019-01-18:21:10

--- python/scripts/src/trim2.py
+++ python/scripts/src/trim2.py
@@ -7,12 +7,12 @@
 import os, sys, string
 
 def quitaCadena(cadena, cadenaQuitar=' '):
-   res=''
-   palabras=cadena.split(cadenaQuitar)
-   for palabra in palabras:
-      if (len(palabra)>0):
-         res+=palabra[0].upper()+palabra[1:]
-   return res
+    res=''
+    palabras=cadena.split(cadenaQuitar)
+    for palabra in palabras:
+        if (len(palabra)>0):
+            res+=palabra[0].upper()+palabra[1:]
+    return res
 
 def seqNumber(leng=2,sep="."):
     i=0
@@ -23,85 +23,98 @@ def seqNumber(leng=2,sep="."):
             res='0'+res
         yield res+sep
 
-def renombraRecursivo(quitar=' ',recursivo=False,reemplazar='',capitalizar=False,renumerar=False,all=False):
-   number=seqNumber()
-   listDir=os.listdir(os.getcwd())
-   listDir.sort()
-   for fname in listDir:
-      #print fname
-      nombre=fname
-      if (fname.find(quitar)>=0):
-         #nombre=quitaCadena(fname,quitar)
-         if (quitar==' ' and capitalizar):
+def renombrarFichero(fname,quitar,reemplazar,capitalizar,renumerar,number):
+    nombre=fname
+    if (fname.find(quitar)>=0):
+        #nombre=quitaCadena(fname,quitar)
+        if (quitar==' ' and capitalizar):
             index=0
             while (fname[index]==quitar):
                 index+=1
             primeraLetra=fname[index]
-            print("primeraLetra: "+primeraLetra)
+            #print("primeraLetra: "+primeraLetra)
             nombre=string.replace(string.capwords(fname,' '),quitar,reemplazar)
             nombre=primeraLetra+nombre[1:]
-         else:
+        else:
             nombre=string.replace(fname,quitar,reemplazar)
-      if (os.path.isdir(nombre) and recursivo):
-         os.chdir(nombre)
-         renombraRecursivo(quitar,recursivo,reemplazar,capitalizar,renumerar,all)
-         os.chdir("..")
-      if (not os.path.isdir(nombre) or all):
-         if (renumerar):
-            nombre=number.next()+nombre
-         if (nombre!=fname):
-            print os.getcwd()+'\\'+fname,'-->',os.getcwd()+'\\'+nombre
-            os.rename(fname,nombre)
+    if (renumerar):
+        nombre=number.next()+nombre
+    if (nombre!=fname):
+        print os.getcwd()+'\\'+fname,'-->',os.getcwd()+'\\'+nombre
+        os.rename(fname,nombre)
 
-      #if (fname!=quitaAcentos(fname)):
-      #   print os.getcwd()+'\\'+fname,'-->',os.getcwd()+'\\'+quitaAcentos(fname)
-      #   os.rename(fname,quitaAcentos(fname))
-      #print(fname)
+def renombraRecursivo(quitar=' ',recursivo=False,reemplazar='',capitalizar=False,renumerar=False,todo=False):
+    number=seqNumber()
+    listDir=os.listdir(os.getcwd())
+    listDir.sort()
+    for fname in listDir:
+        #print fname
+        if (os.path.isdir(fname) and recursivo):
+            os.chdir(fname)
+            renombraRecursivo(quitar,recursivo,reemplazar,capitalizar,renumerar,todo)
+            os.chdir("..")
+            if (not todo): continue
+        renombrarFichero(fname,quitar,reemplazar,capitalizar,renumerar,number)
+
+        #if (fname!=quitaAcentos(fname)):
+        #   print os.getcwd()+'\\'+fname,'-->',os.getcwd()+'\\'+quitaAcentos(fname)
+        #   os.rename(fname,quitaAcentos(fname))
+        #print(fname)
 
 def uso():
-   print("Uso: "+sys.argv[0]+" [-r] [-q <cadena-a-quitar>]")
-   print("Uso: "+sys.argv[0]+" [-r] [-R <cadena-a-reemplazar> <reemplazo>]")
-   print("-r recursivo")
-   print("-q quitar <cadena-a-quitar> de los nombres de los ficheros en lugar de los espacios")
-   print("-A actuar también sobre los directorios")
-   print("-C capitalizar")
-   print("-N renumerar los ficheros 01...0n")
+    print("Uso: "+sys.argv[0]+" [-r] [-q <cadena-a-quitar>]")
+    print("Uso: "+sys.argv[0]+" [-r] [-R <cadena-a-reemplazar> <reemplazo>]")
+    print("-r recursivo")
+    print("-q quitar <cadena-a-quitar> de los nombres de los ficheros en lugar de los espacios")
+    print("-A actuar también sobre los directorios")
+    print("-C capitalizar")
+    print("-N renumerar los ficheros 01...0n")
       
 recursivo=False
 quitar=' ' 
 reemplazar=''   
 capitalizar=False
 renumerar=False
-i=0
+todo=False
+i=1
 
 print "Opciones: "
 while i <len(sys.argv):
-   param=sys.argv[i]
-   if (param=='-r'):
-      print "Recursivo"
-      recursivo=True
-   elif (param=='-R'):
-      i+=1;
-      quitar=sys.argv[i];
-      i+=1;
-      reemplazar=sys.argv[i];
-      print 'Reemplazar: '+quitar+' por '+reemplazar
-   elif (param=='-q'):
-      i+=1;
-      quitar=sys.argv[i];
-      print "Quitar: "+quitar
-   elif (param=='-C'):
-      capitalizar=True
-      print "Capitalizar"
-   elif (param=='-N'):
-      renumerar=True
-      print "Renumerar"
-   elif (param=="-A"):
-      all=True
-   elif (param=='-h' or param=='-help' or param=='--help'):
-      uso();
-      exit(-1);
-   i+=1
-
-renombraRecursivo(quitar,recursivo,reemplazar,capitalizar,renumerar,all)
+    param=sys.argv[i]
+    print("param: "+str(param))
+    if (param=='-r'):
+        print "Recursivo"
+        recursivo=True
+    elif (param=='-R'):
+        i+=1;
+        quitar=sys.argv[i];
+        i+=1;
+        reemplazar=sys.argv[i];
+        print 'Reemplazar: '+quitar+' por '+reemplazar
+    elif (param=='-q'):
+        i+=1;
+        quitar=sys.argv[i];
+        print "Quitar: "+quitar
+    elif (param=='-C'):
+        capitalizar=True
+        print "Capitalizar"
+    elif (param=='-N'):
+        renumerar=True
+        print "Renumerar"
+    elif (param=="-A"):
+        todo=True
+    elif (param=='-h' or param=='-help' or param=='--help'):
+        uso();
+        exit(-1);
+    else: 
+        break
+    i+=1
+if (i<len(sys.argv)):
+    arch=sys.argv[i]
+    if (os.path.isdir(arch)):
+        os.chdir(arch)
+    elif (os.path.isfile(arch)):
+        renombrarFichero(arch, quitar, reemplazar, capitalizar, renumerar, seqNumber())
+        sys.exit(0)
+renombraRecursivo(quitar,recursivo,reemplazar,capitalizar,renumerar,todo)
 
--- scripts/scriptsCeuta/despiertaEpoptes.sh
+++ scripts/scriptsCeuta/despiertaEpoptes.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
+[ -e $(dirname $0)/funcionesAula.sh ] && . /funcionesAula.sh
+[ -e /scripts/scriptsCeuta/funcionesAula.sh ] && . /scripts/scriptsCeuta/funcionesAula.sh
 
+main() {
+    [ "$(ip a | grep 172.124.116)" ] && RED=172.124.116 && MACS="macsCiclo1"
+    [ "$(ip a | grep 172.124.117)" ] && RED=172.124.117 && MACS="macsCiclo2"
+    [ "$1" = "ciclo1" ] && RED=172.124.116 && MACS="macsCiclo1"
+    [ "$1" = "ciclo2" ] && RED=172.124.117 && MACS="macsCiclo2"
+    [ "$1" != "" ] && reiniciaEpoptes $1 && terminarSsh && exit 0
+    #IPS=$(sudo arp-scan -r 5 ${RED}.0/24 | grep $RED | awk '{print $1;}' | sort | uniq)
+    for IP in $($MACS  | awk '{print $1;}'); do 
+        reiniciaEpoptes $IP &
+    done    
+    terminarSsh
+}
 
-[ "$(ip a | grep 172.124.116)" ] && RED=172.124.116 || RED=172.124.117
-IPS=$(sudo arp-scan -r 5 ${RED}.0/24 | grep $RED | awk '{print $1;}' | sort | uniq)
+reiniciaEpoptes() {
+    IP="$1"
+    while true; do 
+        ([ "$IP" = "172.124.117.100" ] || [ "$IP" = "server" ]) && continue
+        PID=$(ssh -p 22 profesor@${IP} ps aux 2>/dev/null  | grep -i epoptes | grep -v root | grep -v profesor | awk '{print $2;}')
+        [ "$PID" = "" ] && break
+        echo ssh -p 22 profesor@${IP} sudo kill -9 $PID
+        ssh -p 22 profesor@${IP} sudo kill -9 $PID 2>/dev/null
+    done
+    WHO=$(ssh $IP who 2>/dev/null | awk -F':' '{print $1;}')
+###    echo "IP: $IP, WHO: $WHO"
+    [ "$WHO" != "" ] && echo ssh -p 22 profesor@$IP sudo "sudo -u $WHO bash -c 'export DISPLAY=:0 && epoptes-client'"
+    [ "$WHO" != "" ] && ssh -p 22 profesor@$IP sudo "sudo -u $WHO bash -c 'export DISPLAY=:0 && epoptes-client&> /dev/null &'" 2>/dev/null &
+}    
 
-for IP in $IPS; do 
-    echo ssh -p 22 profesor@$IP sudo "sudo -u ciclomedio bash -c 'export DISPLAY=:0 && epoptes-client'"
-    ssh -p 22 profesor@$IP sudo "sudo -u ciclomedio bash -c 'export DISPLAY=:0 && epoptes-client&> /dev/null &'" &
-done    
-PIDS=$(ps aux | grep -i ssh | grep profesor | grep $RED | grep DISPLAY | grep 'epoptes-client' | grep -v grep | awk '{print $2;}')
-for P in $PIDS; do 
-   kill -term $P
-done   
+terminarSsh() {
+    PIDS=$(ps aux | grep -i ssh | grep profesor | grep $RED | grep DISPLAY | grep 'epoptes-client' | grep -v grep | awk '{print $2;}')
+    for P in $PIDS; do 
+       kill -term $P
+    done   
+}    
+(return 2>/dev/null) || main "$@"
