790b74855b4fefcdc42ee75ed517f3b4ce04b083
usuario <fran.nucia@gmail.com>
2019-01-19 14:52:38 +0100

    Actualización 2019-01-19:13:52

--- scripts/youtube-dl.sh
+++ scripts/youtube-dl.sh
@@ -9,5 +9,7 @@ else ITEMS=""; fi
 #ITEMS=$(echo $ITEMS | sed "s/-/\\\-/g")
 #youtube-dl --write-srt --sub-lang es,en --sub-format srt -f bestvideo+bestaudio https://www.youtube.com/watch?v=Zh3Yz3PiXZw&feature=youtu.be
 #--write-auto-sub
+#youtube-dl --write-srt --sub-lang es,en --sub-format srt -f bestvideo+bestaudio --auto-number -f bestvideo+bestaudio https://www.youtube.com/playlist?list=PLq1noKggzASu92gX_ARJRk-W9aX_4OL7d
+#youtube-dl --playlist-start 54 --write-srt --sub-lang es,en --sub-format srt -f bestvideo+bestaudio --auto-number --autonumber-start 54 -f bestvideo+bestaudio https://www.youtube.com/playlist?list=PLq1noKggzASu92gX_ARJRk-W9aX_4OL7d
 echo youtube-dl -f bestaudio $ITEMS --extract-audio --audio-format mp3 --output "%(autonumber)s%(title)s.%(ext)s" \'$1\'
 youtube-dl -f bestaudio $ITEMS --extract-audio --audio-format mp3 --output '%(autonumber)s%(title)s.%(ext)s' $1
